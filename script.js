f33395655a0d573bc34c6dd6da16960b37c739a8
MazanovXetai <Mazanovxetai@gmail.com>
2025-06-14 11:06:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeLanguage(lang) {
+  alert(`Dil "${lang.toUpperCase()}" olaraq dəyişdirildi! (Bu funksiya hələ təməl mərhələdədir)`);
+  // Gələcəkdə burda saytdakı yazıları həmin dilə dəyişmək kodları olacaq.
+}
