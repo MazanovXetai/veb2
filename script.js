fe2173ab5378d13edb986b2f657b11c2d916b5da
MazanovXetai <Mazanovxetai@gmail.com>
2025-06-14 11:29:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function changeLanguage(lang) {
-  alert(`Dil "${lang.toUpperCase()}" olaraq dəyişdirildi! (Bu funksiya hələ təməl mərhələdədir)`);
-  // Gələcəkdə burda saytdakı yazıları həmin dilə dəyişmək kodları olacaq.
+  alert(`Dil "${lang.toUpperCase()}`);
 }
+
